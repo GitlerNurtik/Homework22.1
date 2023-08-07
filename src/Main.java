aa38a8cba99a50413d481b685dacd849ba748d20
Nurtik <nurtegingamer@gmail.com>
2023-08-07 21:35:25 +0300

    up

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Places[] places = Places.values();
-        for (int i = 0; i < places.length; i++) {
-            System.out.println(places[i]);
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        try {
+            Places places = Places.valueOf(word.toUpperCase());
+            switch (places) {
+                case LONDON:
+                    System.out.println(places);
+                    break;
+                case PARIS:
+                    System.out.println(places);
+                    break;
+                default:
+                    System.out.println("Myndai jok");
+//                case AUTUMN -> System.out.println("Kuz");
+//                case SUMMER -> System.out.println();
+//                default -> System.out.println();
+            }
+        }catch (IllegalArgumentException e){
+            System.out.println("Myndai enum jok");
         }
     }
 }
